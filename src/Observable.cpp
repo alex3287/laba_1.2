641ea164d4fd84bba426ce1660e9d8882b4baf61
Alexander Mangazeev <alex3287@bk.ru>
2021-01-04 23:06:18 +0700

    add Observable class

---
+++ src/Observable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Александр Мангазеев on 02.01.2021.
+//
+
+#include "Observable.h"
---
+++ src/Observable.h
@@ -0,0 +1,16 @@
+//
+// Created by Александр Мангазеев on 02.01.2021.
+//
+
+#ifndef LABA_1_2_OBSERVABLE_H
+#define LABA_1_2_OBSERVABLE_H
+
+
+
+class Observable {
+
+};
+
+
+
+#endif //LABA_1_2_OBSERVABLE_H
