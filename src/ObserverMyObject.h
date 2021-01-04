a80a3fc193418ea8acde29a3f13de2840d5e8730
Alexander Mangazeev <alex3287@bk.ru>
2021-01-05 01:33:22 +0700

    create class and add method

---
+++ src/ObserverMyObject.h
@@ -0,0 +1,16 @@
+//
+// Created by Александр Мангазеев on 05.01.2021.
+//
+
+#ifndef LABA_1_2_OBSERVERMYOBJECT_H
+#define LABA_1_2_OBSERVERMYOBJECT_H
+#include "Observable.h"
+#include "IFile.h"
+
+class ObserverMyObject: public Observable<IFile> {
+public:
+    void checkingCurrentStatus();
+};
+
+
+#endif //LABA_1_2_OBSERVERMYOBJECT_H
